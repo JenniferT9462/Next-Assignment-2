2fd9439eed02b0f5035a5741f1b18d8ba0c45f7b
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-11 13:04:36 -0500

    Reset, double increment and decrement

--- src/pages/sheep/index.js
+++ src/pages/sheep/index.js
@@ -2,20 +2,41 @@ import { useState } from 'react';
 
 export default function MyCounterApp() {
   const [count, setCount] = useState(0);
+  //Set the state for the inputs
+  // const [inputValue, setInputValue] = useState('');
+  //Function to increase count by 1
   function increment() {
     setCount(count + 1)
-  }
-  
+  };
   function decrement() {
-    setCount(Math.max(count - 1, 0)) //Math.max to set the decrement with a max of 0
-  }
-    return (
+    //Math.max to set the decrement with a max of 0
+    setCount(Math.max(count - 1, 0)) 
+  };
+  //Arrow function to reset count to 0
+  const reset = () => {
+    setCount(0);
+  };
+  function doubleIncrement() {
+    setCount(count + count)
+  };
+  function doubleDecrement() {
+    setCount(count - count)
+  };
+  
+  
+  return (
       <div>
         <h1>My Counter App</h1>
         <h2>Count: {count}</h2>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
+        <br/>
+        <button onClick={doubleIncrement}>Double Increment</button>
+        <button onClick={doubleDecrement}>Double Decrement</button>
+        <br/>
+        
+        <button onClick={reset}>Reset</button>
       </div>
     )
-}
+};
 
